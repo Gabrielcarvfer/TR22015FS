0eff1e6a742fbf8cc27fb4a67419594487afce63
GustavodeAlmeida <gustavodealmeida91@gmail.com>
2015-06-01 18:53:21 -0300

    Identificar o nome do usuário e cria nova página, mainpage, onde a maior parte do processamento acontecerá

---
+++ SyncFiles/src/syncfiles/webserver/WebPage.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package syncfiles.webserver;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class WebPage {
+    String userName = null;
+    
+     public WebPage(String httpContent) throws Exception {
+         setUsername(httpContent);
+         updatePage();
+     }
+
+    private void setUsername(String httpContent){
+       userName = httpContent.substring(5);   
+    }
+
+    public void updatePage(){
+    PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("webpage/mainpage.html", "UTF-8");
+            writer.println("<!DOCTYPE html>");
+            writer.println("<html>");
+            writer.println("<head> Bem vindo,"+userName+ "!</head>");
+            writer.println("<body></body>");
+            writer.println("</html>");
+            writer.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(WebPage.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(WebPage.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            writer.close();
+        }
+    
+    
+    }
+    
+    
+    public void printUsername(){
+    System.out.print(userName);
+    }
+    
+}
