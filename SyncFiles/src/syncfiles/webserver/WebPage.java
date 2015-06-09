d480bedda59bc0aaa1b273b390f8befeefc058bf
GustavodeAlmeida <gustavodealmeida91@gmail.com>
2015-06-08 21:35:48 -0300

    Campos de Criar dir e add file iniciados

--- SyncFiles/src/syncfiles/webserver/WebPage.java
+++ SyncFiles/src/syncfiles/webserver/WebPage.java
@@ -8,6 +8,7 @@ package syncfiles.webserver;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,24 +18,52 @@ import java.util.logging.Logger;
  */
 public class WebPage {
     String userName = null;
+    PrintWriter writer = null;
     
      public WebPage(String httpContent) throws Exception {
-         setUsername(httpContent);
+         processContent(httpContent);
          updatePage();
      }
 
-    private void setUsername(String httpContent){
-       userName = httpContent.substring(5);   
+    private void processContent(String httpContent){
+         if(httpContent.contains("user"))
+        {
+        userName = httpContent.substring(5);
+        }
     }
 
     public void updatePage(){
-    PrintWriter writer = null;
+   
         try {
             writer = new PrintWriter("webpage/mainpage.html", "UTF-8");
             writer.println("<!DOCTYPE html>");
             writer.println("<html>");
             writer.println("<head> Bem vindo,"+userName+ "!</head>");
-            writer.println("<body></body>");
+            writer.println("<body>");
+            writer.println("<div> <p>Criar novo diretório</p>");
+            writer.println("<form  method=\"POST\" action=\"mainpage.html\">");
+            writer.println("<p>Nome do pai do novo diretório:");
+            writer.println("<select name=\"dir\">");
+            printDir();
+            writer.println("</select></p>");
+            writer.println("<p>Nome do novo diretório:<input type=\"text\"name=\"dirName\"></p>");
+            writer.println("<input type=\"submit\" value=\"Submit\">");
+            writer.println("</form>");
+            writer.println("</div>"); 
+            
+            writer.println("<div> <p>Adicionar arquivo</p>");
+            writer.println("<p>Diretório onde o arquivo vai ser alocado:</p>");
+            writer.println("<form  method=\"POST\" action=\"mainpage.html\">");
+            writer.println("<select name=\"fileDir\">");
+            writer.println("<option value=\"Arquivos\">Arquivos</option>");
+            writer.println("</select></p>");
+            writer.println("<p>Nome do novo diretório:<input type=\"text\"name=\"fileeName\"></p>");
+            writer.println("<input type=\"submit\" value=\"Submit\">");
+            writer.println("</form>");
+            writer.println("</div>"); 
+            
+                       
+            writer.println("</body>");
             writer.println("</html>");
             writer.close();
         } catch (FileNotFoundException ex) {
@@ -44,10 +73,13 @@ public class WebPage {
         } finally {
             writer.close();
         }
+        }
     
+    private void printDir(){
+     writer.println("<option value=\"nenhum\">Não desejo criar novo diretório</option>");
+     writer.println("<option value=\"pai\">Arquivos</option>");
     
     }
-    
     
     public void printUsername(){
     System.out.print(userName);
